5732c79bf02038f3bdfc4799a9af33c2c9782d7e
Dean Gardiner <gardiner91@gmail.com>
2013-12-06 15:22:29 +1300

    Restructured repository, created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ASIO',
+    version='1.0',
+    package_dir={'': 'src'},
+    packages=[''],
+    url='https://github.com/fuzeman/ASIO',
+    license='',
+    author='Dean Gardiner',
+    author_email='gardiner91@gmail.com',
+    description=''
+)
